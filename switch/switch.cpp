128ba5a525a12b3ec8d3012b2db65683d5b4562f
Michał Sobolewski <michucity17@gmail.com>
2023-10-08 00:05:16 +0200

    Switch

---
+++ switch/switch.cpp
@@ -0,0 +1,28 @@
+//wykorzystujac 3 metody przekazywania do funkcji utworz 
+//program z menu (switch) ktory umozliwi dodawanie dwoch liczb
+//wykorzystujace przez wartosc przez wskaznik i przez referencje
+
+#include <iostream>
+using namespace std; 
+
+void
+
+int main() { 
+	int x; // wybor w menu 
+	cout <<"Podaj liczbe od 1 do 3: " <<endl; 
+	cin >> x; 
+	switch(x){
+		case 1: 
+		
+		break;
+		
+		case 2: 
+		
+		break; 
+		
+		case 3: 
+		
+		break;
+	}
+return 0; 
+} 
